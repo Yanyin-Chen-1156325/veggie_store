64a933e1ab58cbf2f318d4477cca0a2bd33d4371
machi24 <166906666+machi24@users.noreply.github.com>
2024-10-30 11:15:07 +1300

    Delete config_prod.py

--- config_prod.py
+++
@@ -1,9 +0,0 @@
-# DB configuration
-dbuser = "annchen1156325"
-dbpass = "comp642store"
-dbhost = "annchen1156325.mysql.pythonanywhere-services.com" 
-dbport = "3306"
-dbname = "annchen1156325$storedb"
-
-# debug
-DEBUG = True
